336a72953e31bfc206401fc0989f5f9be4356711
Unaccomplished_Fun05 <150684514+Harshalsharma05@users.noreply.github.com>
2025-05-23 15:52:28 +0530

    github data added in /github

--- index.js
+++ index.js
@@ -6,6 +6,43 @@ const app = express() // Creating an instance of express
 
 const port = 4000 // Defining the port number server will listen on 3000 port
 
+const githubData = {
+
+  "login": "Harshalsharma05",
+  "id": 150684514,
+  "node_id": "U_kgDOCPtDYg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/150684514?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Harshalsharma05",
+  "html_url": "https://github.com/Harshalsharma05",
+  "followers_url": "https://api.github.com/users/Harshalsharma05/followers",
+  "following_url": "https://api.github.com/users/Harshalsharma05/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Harshalsharma05/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Harshalsharma05/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Harshalsharma05/subscriptions",
+  "organizations_url": "https://api.github.com/users/Harshalsharma05/orgs",
+  "repos_url": "https://api.github.com/users/Harshalsharma05/repos",
+  "events_url": "https://api.github.com/users/Harshalsharma05/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Harshalsharma05/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "HarshalSharma_07",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 6,
+  "public_gists": 0,
+  "followers": 2,
+  "following": 1,
+  "created_at": "2023-11-13T13:09:57Z",
+  "updated_at": "2025-05-23T09:59:40Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -20,6 +57,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) => {
     res.send("<h2>You are in the world of youtube!</h2>")
+})
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
