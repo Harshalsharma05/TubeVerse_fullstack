70f8b5dbfd3c1e046240fd5f218d2a6ac4fd089c
Unaccomplished_Fun05 <150684514+Harshalsharma05@users.noreply.github.com>
2025-05-30 19:47:09 +0530

    Add refresh token functionality - Implemented refreshAccessToken function to handle token refresh requests. - Added refresh token route to user routes.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,8 @@ import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import jwt from "jsonwebtoken"
+
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
@@ -26,6 +28,16 @@ const generateAccessAndRefreshTokens = async (userId) => {
         throw new ApiError(500, "Something went wrong while generating the tokens")
     }
 }
+
+// register user logic:
+// get the data from frontend and check all the fields if given (validate - not empty)
+// check in db if user already exists by email and username or not if yes then just return
+// check for images, check for avatar (as required field) 
+// upload them to cloudinary, check again if avatar is uploaded because it is required field
+// create user object - create entry in db
+// remove password and refresh token field from response
+// check for user creation
+// return response
 
 // why asyncHandler is used?
 // asyncHandler is a utility function that wraps asynchronous route handlers to catch errors and pass them to the next middleware, preventing the need for try-catch blocks in every route handler.
@@ -112,18 +124,22 @@ const registerUser = asyncHandler( async (req, res) => {
     )
 })
 
+
 // steps in login
 // req body -> data
 // username or email
 // find the data
 // password check
 // access and refresh token
-// send secure coookie
-
+// send secure coookie (hhtp only and secure true)
 
 const loginUser = asyncHandler ( async(req, res) => {
 
     const { username, email, password } = req.body
+
+    // if(!username && !email){
+    //     throw new ApiError(400, "Username or email is required")
+    // }
 
     if( !(username || email) ){
         throw new ApiError(400, "Username or email is required")
@@ -146,7 +162,7 @@ const loginUser = asyncHandler ( async(req, res) => {
 
     const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
 
-    const loggedInUser = User.findById(user._id).
+    const loggedInUser = await User.findById(user._id).
     select("-password -refreshToken")
 
     const options = { // secured cookie: by this cookie could be accessed by the backend server only, not the frontend
@@ -170,7 +186,7 @@ const loginUser = asyncHandler ( async(req, res) => {
 
 const logoutUser = asyncHandler( async(req, res) => {
 
-    await User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user._id, // this is what we have created object (.user) in the verifyJWT in auth middleware
         
         // what to update
@@ -190,25 +206,71 @@ const logoutUser = asyncHandler( async(req, res) => {
         secure: true 
     }
 
+    const loggedInUser = await User.findById(user._id).select("username -_id")
+
     return res
     .status(200)
     .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
-    .json(new ApiResponse(200, {}, "User logged out"))
+    .json(new ApiResponse(200, {loggedInUser}, "User logged out"))
 })
+
+const refreshAccessToken = asyncHandler( async(req, res) => {
+    
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if(!incomingRefreshToken) {
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    // for safety purposes because maybe while decoding token it may throw some error (not neccessary)
+    try { 
+        const decodedToken = jwt.verify(
+            incomingRefreshToken, 
+            process.env.REFRESH_TOKEN_SECRET
+        )
+    
+        const user = await User.findById(decodedToken?._id)
+    
+        if(!user) {
+            throw new ApiError(401, "Invalid Refresh Token")
+        }
+    
+        if(incomingRefreshToken !== user?.refreshToken){
+            throw new ApiError(401, "Refresh Token is expired or used")
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        // generate new tokens
+        const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(user._id)
+        
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    accessToken,
+                    refreshToken: newRefreshToken
+                },
+                "Access Token refreshed"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+})
+
 
 export { 
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 }
-
-// register user logic:
-// get the data from frontend and check all the fields if given (validate - not empty)
-// check in db if user already exists by email and username or not if yes then just return
-// check for images, check for avatar (as required field) 
-// upload them to cloudinary, check again if avatar is uploaded because it is required field
-// create user object - create entry in db
-// remove password and refresh token field from response
-// check for user creation
-// return response
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
-import { ApiError } from "../utils/apiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
-import { User } from "../models/user.model";
+import { User } from "../models/user.model.js";
 
 // here res in not used anywhere so we can replace it with underscore (_) (good practice)
 export const verifyJWT = asyncHandler( async(req, _, next) => { 
