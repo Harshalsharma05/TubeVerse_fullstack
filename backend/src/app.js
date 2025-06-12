43e62d12bf86495c35dabde8f08c49ec6c1d45c5
Unaccomplished_Fun05 <150684514+Harshalsharma05@users.noreply.github.com>
2025-06-13 01:43:30 +0530

    feat: Update CORS configuration to allow requests from the production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middlewares setup
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://tube-verse-fullstack-mern.vercel.app/"],
     credentials: true,
 }))
 
